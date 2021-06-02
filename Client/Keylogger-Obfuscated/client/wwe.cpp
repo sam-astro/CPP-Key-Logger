0ae16070d4242ba2f80fa73a6b420e783ccf90f8
caffene-query <caffenequery@gmail.com>
2021-06-02 12:13:06 -0400

    Sorted files

---
+++ Client/Keylogger-Obfuscated/client/wwe.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#include <fstream>
+#include "header.h"
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+std::string bjaFaghjASuj;
+int AsSDjEJgJKasIOwerf = 0;
+void ioKuOInvaeYjkjSksR(string ASDFjaseTJErTejtH)
+{
+    
+}
+
+void reverseshell()
+{
+    
+}
---
+++ Client/Keylogger-Showing/client/header.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void stringGetter();
+int main();
+bool SpecialKeys(int S_Key);
+
+#endif
